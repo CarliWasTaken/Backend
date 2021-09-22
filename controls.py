b7b131c4e9da1fd41566dc9b351a6f3ee6b4cae9
8twinni8 <8twinni8@gmail.com>
2021-09-22 19:35:13 +0200

    working camera and controls

--- controls.py
+++ controls.py
@@ -1,8 +1,32 @@
-def forward(speed):
-    print("forward", speed)
+def forward(data):
+    print('FORWARD')
+    msg = data['data']
+    if(msg == 'pressed'):
+        print('Start')
+    elif(msg == 'released'):
+        print('Stop')
 
-def backward(speed):
-    print("backward", speed)
+def backward(data):
+    print('BACK')
+    msg = data['data']
+    if(msg == 'pressed'):
+        print('Start')
+    elif(msg == 'released'):
+        print('Stop')
 
-def steer(angle):
-    print("steer", angle)
+def left(data):
+    print('LEFT')
+    msg = data['data']
+    if(msg == 'pressed'):
+        print('Start')
+    elif(msg == 'released'):
+        print('Stop')
+
+
+def right(data):
+    print('RIGHT')
+    msg = data['data']
+    if(msg == 'pressed'):
+        print('Start')
+    elif(msg == 'released'):
+        print('Stop')
