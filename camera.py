38230ba05c84a772c2e743d0b1b7fafbd63e4350
8twinni8 <8twinni8@gmail.com>
2021-09-25 15:58:53 +0200

    trying to implement dual webcam (wip)

--- camera.py
+++ camera.py
@@ -1,4 +1,29 @@
 import cv2
+import numpy as np
+
+class Camera:
+    def __init__(self, camera):
+        self.camera = cv2.VideoCapture(camera)
+
+    def capture_frame(self):
+        success, self.frame = self.camera.read()
+
+    def create_ai_image(self):
+        self.frame = cv2.addWeighted(self.frame, 5, np.zeros(self.frame.shape, self.frame.dtype), 0, 0)
+
+    def get_bytes(self):
+        return cv2.imencode('.jpg', self.frame)[1].tobytes()
+
+def capture_video():
+    pass
+
+def gen_ai_frames():
+    c = Camera(1)
+    while True:
+        c.capture_frame()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + c.get_bytes() + b'\r\n') 
+        print(c.get_bytes())
 
 def gen_frames():
     camera = cv2.VideoCapture(1)
--- carli.py
+++ carli.py
@@ -1,14 +1,14 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_socketio import SocketIO, emit
 import logging
 import controls
-import camera
+from camera import Camera
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
-
+streaming = False
 
 @socketio.on('forward', namespace='/drive')
 def handle_forward(data):
@@ -26,10 +26,42 @@ def handle_left(data):
 def handle_right(data):
     controls.right(data)
 
-@app.route('/video')
-def video_feed():
-    return Response(camera.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# On client connect fire this message
+@socketio.on('connect', namespace='/stream')
+def handle_stream_connect():
+    # Sends a test message to the user when connecting
+    emit('ai', 'Test1')
+    pass
 
+# Should deactivate the while loop is client disconnects (doesn't work or does it?)
+@socketio.on('disconnect', namespace='/stream')
+def handle_stream_disconnect():
+    global streaming
+    streaming = False
+    print('Streaming ended!')
+    pass
+
+# Starts the stream to the user
+@socketio.on('start_stream', namespace='/stream')
+def handle_start_stream(data):
+    global streaming
+    streaming = True
+    print('Starting streaming...')
+    stream()
+
+# Streaming function for the video
+def stream():
+    # create Camera with the cameraId
+    c = Camera(1)
+    global streaming
+    while streaming:
+        # captures an image
+        c.capture_frame()
+        # send picture bytes to the user
+        emit('video', c.get_bytes())
+        c.create_ai_image()
+        emit('ai', c.get_bytes())
+        
 if __name__ == '__main__':
     print("Starting server")
     socketio.run(app, port=5000, host='0.0.0.0')
