4d5473f6c421df135f0d9bde9d1e6e5c01a45c10
Ionas <seijod17@htl-kaindorf.at>
2021-10-12 11:47:48 +0200

    Deleted unnecessary stuff

--- carli.py
+++ carli.py
@@ -1,30 +1,12 @@
 from flask import Flask, Response
 from flask_socketio import SocketIO, emit
 import logging
-import controls
 import camera
 
 # log = logging.getLogger('werkzeug')
 # log.setLevel(logging.ERROR)
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
-
-
-@socketio.on('forward', namespace='/drive')
-def handle_forward(data):
-    controls.forward(data)
-
-@socketio.on('backward', namespace='/drive')
-def handle_backward(data):
-    controls.backward(data)
-
-@socketio.on('left', namespace='/drive')
-def handle_left(data):
-    controls.left(data)
-
-@socketio.on('right', namespace='/drive')
-def handle_right(data):
-    controls.right(data)
 
 @app.route('/video')
 def video_feed():
